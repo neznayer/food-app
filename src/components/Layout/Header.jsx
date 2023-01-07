fa7ace0def72ae00772524fd4ee299e28129e788
Anton <nokker@gmail.com>
2023-01-07 14:55:35 +0900

    Add meals basic

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,17 @@
+import image from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+import { HeaderCartButton } from "./HeaderCartButton";
+
+export const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={image} alt="A table full of delicious food" />
+      </div>
+    </>
+  );
+};
